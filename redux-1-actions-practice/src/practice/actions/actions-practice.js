2a61ad568508e75b48a8dd40effacc93ef3799a1
Sean Tayler <seanjtayler@gmail.com>
2018-11-05 11:52:50 -0700

    practice tests complete

--- redux-1-actions-practice/src/practice/actions/actions-practice.js
+++ redux-1-actions-practice/src/practice/actions/actions-practice.js
@@ -1,6 +1,6 @@
-/* 
+/*
   EXAMPLE PROBLEM!!!
-  
+
   Export the following:
     - An action type label that says ADD_TODO
     - An action creator function called "addTodo" that expects a new todo object as an argument.
@@ -23,6 +23,13 @@ export const addTodo = (newTodo) => {
     - An action creator function called "increment" that expects no arguments.
 */
 
+export const INCREMENT = 'INCREMENT'
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  }
+}
 
 /*
   Problem # 2
@@ -32,6 +39,14 @@ export const addTodo = (newTodo) => {
     - An action creator function called "addGrocery" that expects one argument "newGroceryItem".
 */
 
+export const ADD_GROCERY = 'ADD_GROCERY'
+
+export const addGrocery = (newGroceryItem) => {
+  return {
+    type: ADD_GROCERY,
+    payload: newGroceryItem
+  }
+}
 
 /*
   Problem # 3
@@ -41,6 +56,14 @@ export const addTodo = (newTodo) => {
     - An action creator function called "updateContact" that expects one argument "updatedContact".
 */
 
+export const UPDATE_CONTACT = 'UPDATE_CONTACT'
+
+export const updateContact = (updatedContact) => {
+  return {
+    type: UPDATE_CONTACT,
+    payload: updatedContact
+  }
+}
 
 /*
   Problem # 4
@@ -50,6 +73,14 @@ export const addTodo = (newTodo) => {
     - An action creator function called "removeUser" that expects one argument "id".
 */
 
+export const REMOVE_USER = 'REMOVE_USER'
+
+export const removeUser = (id) => {
+  return{
+    type: REMOVE_USER,
+    payload: id
+  }
+}
 
 /*
   Problem # 5
@@ -58,3 +89,12 @@ export const addTodo = (newTodo) => {
     - An action type label that says FETCH_USER
     - An action creator function called "fetchUser" that expects one argument "id".
 */
+
+export const FETCH_USER = 'FETCH_USER'
+
+export const fetchUser = (id) => {
+  return {
+    type: FETCH_USER,
+    payload: id
+  }
+}
--- redux-1-actions/src/redux/todos.actions.js
+++ redux-1-actions/src/redux/todos.actions.js
@@ -3,6 +3,18 @@ export const REMOVE_TODO = 'REMOVE_TODO'
 
 // Your job is to write two action creators:
 
-export const addTodo = () => {}
+export const addTodo = (newTodo) => {
+  let action = {
+    type: ADD_TODO,
+    payload: newTodo
+  }
+  return action
+}
 
-export const removeTodo = () => {}
+export const removeTodo = (id) => {
+  let action = {
+    type: REMOVE_TODO,
+    payload: id
+  }
+  return action
+}
